8ebaba7b8cf884f5e27e5ba3108477d12e9a2baf
Kerem Ersoy <azimetkeremersoy@gmail.com>
2024-07-13 15:27:48 +0300

    theme provider defaultTheme set light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={cn("h-screen w-full flex", GeistSans.className)}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
