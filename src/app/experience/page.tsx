f0c780dd344586bc926d9360a8b061894f42a9ce
Kerem Ersoy <azimetkeremersoy@gmail.com>
2024-07-14 12:30:48 +0300

    orbit circle add for tech stack

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,7 +1,15 @@
+import { OrbitCircle } from "@/components/orbit-circle";
 import React from "react";
 
 const ExperiencePage = () => {
-  return <div>experience</div>;
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">Experience</h2>
+      <div>
+        <OrbitCircle />
+      </div>
+    </div>
+  );
 };
 
 export default ExperiencePage;
